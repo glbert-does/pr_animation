e886a9f4087c5af65668a8366e918c2b403f41ad
glbert-does <stefan@elfenbeinturm.at>
2026-01-05 23:24:10 +0100

    be explicit about text colour, remove theme dependency

--- pr_animation.R
+++ pr_animation.R
@@ -1,7 +1,6 @@
 # R 4.4.2
 require(ggplot2) # ggplot
 require(gganimate) # transition_reveal, view_follow, anim_save, animate
-require(ggthemes) # theme_solarized_2
 require(gifski) # creating gif
 require(magick) # image_read_svg, image_modulate
 require(rsvg) # loaded by image_read_svg
@@ -22,21 +21,22 @@ lilogo <- image_read_svg("lichesslogowhite.svg", width=400, height=400) |>
 
 graph <- ggplot(pullRequests, aes(x=month, y=total, group=1)) +
 	annotation_custom(lilogo) +
-	theme_solarized_2(light=FALSE) +
 	ylab("Pull requests") +
 	theme(text=element_text(size=16, colour="#EEEEEE"),
+				axis.text=element_text(colour="#FFFFFF"),
 				axis.title.x=element_blank(),
+				panel.border=element_blank(),
 				panel.background=element_rect(fill=NA),
-				plot.background=element_rect(fill="#161512"),
 				panel.grid.major=element_blank(),
 				panel.grid.minor=element_blank(),
+				plot.background=element_rect(fill="#161512", linetype=0),
 				legend.position="none",
 				plot.margin=unit(c(1, 1, 1, 1), "cm")) +
 	scale_x_date(date_breaks="1 year", date_labels="%Y", expand=c(0, 0, 0.05, 0)) +
-	geom_line(size=2, alpha=.8, color="#A7711C", aes(group=1)) +
+	geom_line(linewidth=2, alpha=.8, color="#A7711C", aes(group=1)) +
 	geom_point(size=2, color="#A7711C")
 
-graph_quarter <- graph %+% aes(y=quarter) + ylab("Monthly PRs (Rolling 3 Month Mean)")
+graph_quarter <- graph + aes(y=quarter) + ylab("Monthly PRs (Rolling 3 Month Mean)")
 
 anim_save("pulls_q.gif",
 					animate(graph_quarter + transition_reveal(month) + view_follow(fixed_y=TRUE),
